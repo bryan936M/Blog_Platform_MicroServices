c74c9c7e57655f2ad9eac2a72ae579379434ec28
Bryan Murasira <bryanmurasira@gmail.com>
2024-01-24 17:43:45 +0200

    chore: database connection setup

---
+++ User/src/Database/index.ts
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/planetscale-serverless";
+import { connect } from "@planetscale/database";
+import { config } from "../Config";
+
+// create the connection
+const connection = connect({
+  url: config.database_URL,
+});
+
+const db = drizzle(connection);
+
+export { db };
