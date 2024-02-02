b7a84525a42f01501869c40276754881772b4593
Bryan Murasira <bryanmurasira@gmail.com>
2024-02-02 13:33:53 +0200

    bug: fixed the db:push error

--- User/src/Database/schema.ts
+++ User/src/Database/schema.ts
@@ -1,12 +1,11 @@
 import { datetime, mysqlTable, serial, varchar } from "drizzle-orm/mysql-core";
 
-const users = mysqlTable("users", {
+export const users = mysqlTable("users", {
   id: serial("id").autoincrement().primaryKey(),
   first_name: varchar("first_name", { length: 250 }).notNull(),
   last_name: varchar("last_name", { length: 250 }).notNull(),
   email: varchar("email", { length: 250 }).unique().notNull(),
   password: varchar("password", { length: 250 }).notNull(),
+  dob: datetime("dob").notNull(),
   created_at: datetime("created_at").notNull(),
 });
-
-export { users };
